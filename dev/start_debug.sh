78e4e1cd71e117b206a6a54f27748259d7bcaac9
Kostas Theodorou <thek27@gmail.com>
2020-08-25 13:58:54 +0300

    Using python 2.7  instead of 3

--- dev/start_debug.sh
+++ dev/start_debug.sh
@@ -1,5 +1,10 @@
 
-apk update && apk add --no-cache git py-pip g++
+apk update && apk add --no-cache git g++ python2
+python -m ensurepip
+pip install --upgrade pip setuptools
+rm -r /root/.cache
+
 go get github.com/go-delve/delve/cmd/dlv
+
 pip install supervisor supervisor-stdout watchdog
 /usr/bin/supervisord -n -c /go/dev/supervisord.conf
